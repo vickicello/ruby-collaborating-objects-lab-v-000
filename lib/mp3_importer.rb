efc3ab88707525d9596e71d0de6632a401000011
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 15:30:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,9 +4,10 @@ attr_accessor :testest_music_path
 
   def initialize(test_music_path)
     @test_music_path = test_music_path
+    @path = path
   end
 
-  def path()
+  
 
     def import
       Song.new_by_filename(some_filename)

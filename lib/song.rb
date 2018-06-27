c53eb885a2278adbf08c207da03fa341171f3a74
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 16:11:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,23 +5,19 @@ class Song
   @@all = []
 
   def initialize(name)
-      @name = name
+    @name = name
   end
 
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-
-    def self.all
-      @@all
-    end
 
   def self.new_by_filename(filename)
-
+    artost, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist_name
+    new_song
   end
 
 end
